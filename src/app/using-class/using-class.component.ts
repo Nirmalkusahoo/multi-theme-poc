0b178bff3517d8bfe1abe1a1fd36ccfb5336a14f
i537736 <nirmal.kumar.sahoo@sap.com>
2024-04-17 10:39:45 +0530

    config changes

--- src/app/using-class/using-class.component.ts
+++ src/app/using-class/using-class.component.ts
@@ -1,16 +1,42 @@
 import {Component, Renderer2} from '@angular/core';
+import {environment} from "../../environments/environment";
+import {CommonModule, NgIf} from "@angular/common";
+import {MatSlideToggleChange, MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-using-class',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatSlideToggleModule,
+  ],
   templateUrl: './using-class.component.html',
   styleUrl: './using-class.component.scss'
 })
 export class UsingClassComponent {
+  public environment = environment;
+  public isDigital:boolean;
+  public isBAH:boolean;
   constructor(private renderer2: Renderer2,) {
   }
 
+  public onDigitalChange(changeValue:MatSlideToggleChange): void {
+    if (changeValue.checked) {
+      this.renderer2.addClass(document.body, 'digital-theme');
+    } else {
+      this.renderer2.removeClass(document.body, 'digital-theme')
+    }
+  }
+
+  public onBAHChange(changeValue:MatSlideToggleChange): void {
+    if (changeValue.checked) {
+      this.renderer2.addClass(document.body, 'bah-theme');
+    } else {
+      this.renderer2.removeClass(document.body, 'bah-theme')
+    }
+  }
   public changeTheme(changeValue: boolean): void {
     if (changeValue) {
       this.renderer2.addClass(document.body, 'digital-theme');
