7bdb507c8b56bbaef52a7514e94edec8ccd09e05
i537736 <nirmal.kumar.sahoo@sap.com>
2024-04-20 00:01:09 +0530

    theme logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, Renderer2} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {FirstComponent} from "./first/first.component";
 import {UsingClassComponent} from "./using-class/using-class.component";
+import {environment} from "../environments/environment";
 
 @Component({
   selector: 'app-root',
@@ -12,4 +13,17 @@ import {UsingClassComponent} from "./using-class/using-class.component";
 })
 export class AppComponent {
   title = 'multi-theme-poc';
+  constructor(private renderer2: Renderer2,) {
+    this.checkForTheme();
+  }
+
+  private checkForTheme(): void {
+    if (environment.showTheme) {
+      const head = document.head;
+      const link = this.renderer2.createElement('link');
+      link.rel = 'stylesheet';
+      link.href = 'first-theme.css';
+      this.renderer2.appendChild(head, link);
+    }
+  }
 }
--- src/app/using-class/using-class.component.ts
+++ src/app/using-class/using-class.component.ts
@@ -1,6 +1,6 @@
 import {Component, Renderer2} from '@angular/core';
 import {environment} from "../../environments/environment";
-import {CommonModule, NgIf} from "@angular/common";
+import {CommonModule} from "@angular/common";
 import {MatSlideToggleChange, MatSlideToggleModule} from "@angular/material/slide-toggle";
 import {FormsModule} from "@angular/forms";
 
@@ -17,39 +17,23 @@ import {FormsModule} from "@angular/forms";
 })
 export class UsingClassComponent {
   public environment = environment;
-  public isDigital:boolean;
-  public isBAH:boolean;
+
   constructor(private renderer2: Renderer2,) {
   }
 
-  public onDigitalChange(changeValue:MatSlideToggleChange): void {
+  public onFirstThemeChange(changeValue: MatSlideToggleChange): void {
     if (changeValue.checked) {
-      this.renderer2.addClass(document.body, 'digital-theme');
+      this.renderer2.addClass(document.body, 'first-theme');
     } else {
-      this.renderer2.removeClass(document.body, 'digital-theme')
+      this.renderer2.removeClass(document.body, 'first-theme')
     }
   }
 
-  public onBAHChange(changeValue:MatSlideToggleChange): void {
+  public onSecondThemeChange(changeValue: MatSlideToggleChange): void {
     if (changeValue.checked) {
-      this.renderer2.addClass(document.body, 'bah-theme');
+      this.renderer2.addClass(document.body, 'second-theme');
     } else {
-      this.renderer2.removeClass(document.body, 'bah-theme')
+      this.renderer2.removeClass(document.body, 'second-theme')
     }
-  }
-  public changeTheme(changeValue: boolean): void {
-    if (changeValue) {
-      this.renderer2.addClass(document.body, 'digital-theme');
-    } else {
-      this.renderer2.removeClass(document.body, 'digital-theme')
-    }
-
-    // this.renderer2.addClass(document.body, 'black_theme');
-    /* this.renderer2.setStyle(
-       this.el.nativeElement.ownerDocument.body,
-       '--clr_light_text',
-       'red',
-     );
-     // this.renderer2.setStyle(document.body, '$clr-light-text', '#0057D2');*/
   }
 }
